3b49a1b4b8496cdb533ad8ed886dab75845ec80f
MatheusRFC <matheusr220998@gmail.com>
2022-04-08 18:12:51 -0300

    Adicionados dois componentes:
    - Tela de Login.
    
    - Tela de Cadastro.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponentComponent } from './components/login-component/login-component.component';
+import { CadastroComponentComponent } from './components/cadastro-component/cadastro-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponentComponent,
+    CadastroComponentComponent
   ],
   imports: [
     BrowserModule,
