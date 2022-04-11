d73e5cb9ddf5db4bbe7bc9e3d1bfb8c64a086c71
MatheusRFC <matheusr220998@gmail.com>
2022-04-11 16:16:19 -0300

    Melhorias adicionadas à tela de cadastro no que se refere a exibição dos possíveis erros.

--- src/app/components/cadastro-component/cadastro-component.component.ts
+++ src/app/components/cadastro-component/cadastro-component.component.ts
@@ -25,6 +25,13 @@ export class CadastroComponentComponent implements OnInit {
   //Função que recebe e verifica os dados de cadastro.
   coleta_Dados(nome: string, senha: string, senha_confirm: string){
 
+    /*
+    Apaga o conteúdo da <div> responsável pelos erros.
+    Essa parte é necessária para evitar acúmulo de erros na tela
+    mesmo com os erros corrigidos.
+    */
+    document.getElementById("campo_erros").innerHTML = "";
+
     //Pega o nome de usuário do localstorage.
     var users = localStorage.getItem(nome);
 
@@ -35,6 +42,11 @@ export class CadastroComponentComponent implements OnInit {
 
     //Caso o usuário não esteja cadastrado, o script o cadastra.
     else{
+
+      // Perga a div "campo_erros" para adicionar os erros caso existam.
+      var mostrar_usuario = document.getElementById("campo_erros");
+      var p = document.createElement("p");
+
       var users = localStorage.getItem(nome);
       //Cria um array de objetos
       const usuario = {
@@ -48,29 +60,31 @@ export class CadastroComponentComponent implements OnInit {
       var error_existe = false;
 
       //Expressão regular responsável por definir os parametros da senha.
-      let regexp = new RegExp("^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*[@#$%^&+=]).*$");
+      let regexp = new RegExp("^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*[@#$%^&+=*>!<_`-]).*$");
 
       //Verifica se o campo nome não está vazio e se ele possui pelo menos 4 caracteres.
       if (nome == "" || nome.length < 4){
-        error = error + "Nome de usuário está vazio ou tem menos de 4 caracteres.\n";
-        error_existe = true;
+        error = error + "• Nome de usuário está vazio ou tem menos de 4 caracteres.";
+        p.textContent = error;
+        mostrar_usuario.appendChild(p);
       }
 
       //Verifica se o campo de senha não esta vazio e se é igual a confirmação
       if (senha != senha_confirm || senha == "" || senha_confirm == ""){
-        error = error + "As senhas tem que ser iguais e não podem ser vazias.\n";
-        error_existe = true;
+        error = "• As senhas tem que ser iguais e não podem ser vazias.";
+
+        var b = document.createElement("p");
+        b.textContent = error;
+        mostrar_usuario.appendChild(b);
       } 
 
       //Verifica se a senha atente aos requisitos mínimos dispostos na expressão regular.
       if (regexp.test(senha) == false){
-        error = error + "As senhas devem conter pelo menos 8 caracteres, uma letra, um número e um caractere especial.\n";
-        error_existe = true;
-      }
+        error = "• As senhas devem conter pelo menos 8 caracteres, uma letra, um número e um caractere especial.";
 
-      //Caso exista um ou mais erros, imprime na tela através de um alert.
-      if (error_existe == true){
-        alert(error);
+        var c = document.createElement("p");
+        c.textContent = error;
+        mostrar_usuario.appendChild(c);
       }
 
       //Caso esteja tudo correto com as informações, exibe uma confirmação de usuário cadastrado.
@@ -78,7 +92,6 @@ export class CadastroComponentComponent implements OnInit {
           alert("Usuário cadastrado com sucesso!");
           localStorage.setItem(nome, JSON.stringify(usuario));
           this.route.navigate(['']);
-        
       }
     }
   }
